03a3f94156297e91e2644e6aa735cf9b9564cfe1
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2026-02-11 00:19:29 +0530

    Update 189_Rotate_Array.py

--- Two_Pointers/189_Rotate_Array.py
+++ Two_Pointers/189_Rotate_Array.py
@@ -4,7 +4,7 @@
 Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 """
 
-# Solution: T.C: O(0); O(1)
+# Solution: T.C: O(N^2); O(1)
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
@@ -16,4 +16,19 @@ class Solution:
                 nums[j]=nums[j-1]
             nums[0]=ele
         
-# Solution: T.C: O(0); O(1)
+# Solution: T.C: O(N); O(1)
+class Solution:
+    def reverse(self, nums, i,j):
+        while (i<=j):
+            nums[i], nums[j]=nums[j], nums[i]
+            i+=1; j-=1
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        self.reverse(nums, 0, len(nums)-1)
+        self.reverse(nums, 0, k-1) 
+        self.reverse(nums, k, len(nums)-1) 
+        
