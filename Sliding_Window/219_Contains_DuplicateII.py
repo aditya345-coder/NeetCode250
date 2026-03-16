000112f001284db8525b1eb16995509e89b18430
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2026-03-16 07:16:59 +0530

    Update 219_Contains_DuplicateII.py

--- Sliding_Window/219_Contains_DuplicateII.py
+++ Sliding_Window/219_Contains_DuplicateII.py
@@ -26,3 +26,17 @@ class Solution:
                 else:
                     hashmap[nums[i]]=i
         return False
+
+## Alternate Solution: Time Complexity: O(n)  Space Complexity: O(1)
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window=set(); l=0
+        for r in range(len(nums)):
+            if (r-l>k):
+                window.remove(nums[l])
+                l+=1
+            if (nums[r] in window):
+                return True
+            window.add(nums[r])
+        return False
+
